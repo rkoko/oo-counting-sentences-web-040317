b44c83b2e625ac4bfced5843562ab90bb6e803fb
rkoko <rinda.ko@gmail.com>
2017-04-05 17:30:34 -0400

    counting sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,32 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+ def helper(string, char)
+      string[-1] == char
+ end
+
+  def sentence?(string = self)
+    helper(string, ".")
   end
 
-  def question?
-
+  def question?(string = self)
+    #self[-1] == "?"
+    helper(string,"?")
   end
 
-  def exclamation?
-
+  def exclamation?(string = self)
+    #self[-1] == "!"
+    helper(string,"!")
   end
 
   def count_sentences
-
+    counter = 0
+    self.split.each do |word|
+      binding.pry
+      if sentence?(word) || question?(word) || exclamation?(word)
+        counter +=1
+      end
+    end
+    counter
   end
 end
